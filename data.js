e0438ee0a48749817dc78d89c4570ea61583e9bb
Lucas Eckman <lucas.eckman@gmail.com>
2019-02-08 14:15:18 -0500

    drawing and dragging and zooming rects

--- data.js
+++ data.js
@@ -1,1 +1,26 @@
-data = {}
+var rects = [
+  {
+    x: 10,
+    y: 10,
+    width: 10,
+    height: 10
+  },
+  {
+    x: 50,
+    y: 10,
+    width: 10,
+    height: 10
+  },
+  {
+    x: 10,
+    y: 50,
+    width: 10,
+    height: 10
+  },
+  {
+    x: 50,
+    y: 50,
+    width: 10,
+    height: 10
+  }
+];
--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("index.js loaded")
+var svg = d3.select("svg"),
+  width = +svg.attr("width"),
+  height = +svg.attr("height"),
+  transform = d3.zoomIdentity;
+
+var g = svg.append("g");
+
+g.selectAll("rect")
+  .data(rects)
+  .enter()
+  .append("rect")
+  .attr("x", function(d) {
+    return d.x;
+  })
+  .attr("y", function(d) {
+    return d.y;
+  })
+  .attr("width", function(d) {
+    return d.width;
+  })
+  .attr("height", function(d) {
+    return d.height;
+  })
+  .call(d3.drag().on("drag", dragged));
+
+svg.call(
+  d3
+    .zoom()
+    .scaleExtent([1 / 2, 8])
+    .on("zoom", zoomed)
+);
+
+function zoomed() {
+  g.attr("transform", d3.event.transform);
+}
+
+function dragged(d) {
+  d3.select(this)
+    .attr("x", (d.x = d3.event.x))
+    .attr("y", (d.y = d3.event.y));
+}
